b11559ea6e42f4841f352c1edf6fc68c13959a3d
Homer White <hwhite0@georgetowncollege.edu>
2014-09-13 14:56:05 -0400

    add package documentation

---
+++ R/classr.R
@@ -0,0 +1,15 @@
+#' classr
+#'
+#' Functions for Course Managment in the R Studio Environment.
+#'
+#'
+#' @name classr-package
+#' @aliases classr classr-package
+#' @docType package
+#' @title Course-Management Functions
+#' @author Homer White (\email{hwhite0@@georgetowncollege.edu}), Scott Switzer (\email{Scott_Switzer@@georgetowncollege.edu})
+#' @references
+#' \url{http://github.com/homerhanumat/classr}
+#'
+#' @keywords package
+NULL
